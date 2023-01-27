144379c63b3d0b86bab280f384706d45179910f5
Emmett Gallic <emmettgallic@gmail.com>
2023-01-27 16:15:27 -0500

    change link to button on index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,27 +23,15 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+def dailyQ(dq, *args):
+    daily_question = Question.objects.create(question_text=dq, pub_date=timezone.now())
+    dq_choices = Choice.objects.create(question=daily_question, choice_text=args)
+
+dailyQ("Did you program yet today?", 'Yes', 'No')
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-
-# Non-generic views
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return render(request, 'polls/index.html', context)
-#
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', { 'question':question})
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return(render(request, 'polls/results.html', {'question': question}))
-
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -62,6 +50,22 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+    # Non-generic views
+    # def index(request):
+    #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #     context = {
+    #         'latest_question_list': latest_question_list,
+    #     }
+    #     return render(request, 'polls/index.html', context)
+    #
+    # def detail(request, question_id):
+    #     question = get_object_or_404(Question, pk=question_id)
+    #     return render(request, 'polls/detail.html', { 'question':question})
+    #
+    # def results(request, question_id):
+    #     question = get_object_or_404(Question, pk=question_id)
+    #     return(render(request, 'polls/results.html', {'question': question}))
 
 
 
